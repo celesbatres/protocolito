0fd03ab90a6c3d79239937f5882e287636391524
Celeste Batres <56047336+celestebatres@users.noreply.github.com>
2024-10-22 09:30:54 -0600

    feat: udp conection, app - driver

--- App/Server.java
+++ App/Server.java
@@ -1,3 +1,4 @@
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -6,9 +7,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 public class Server {
+
     public static void main(String[] args) throws IOException {
         // Crea el servidor en el puerto 8000
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
@@ -18,15 +23,28 @@ public class Server {
         System.out.println("Servidor escuchando en http://localhost:8000/post");
     }
 
-    public static void sendToDriver(String message){
+    public static void sendToDriver(String message) {
         // Abre un cliente udp y manda un mensaje al puerto del driver
         try {
-            
+            int port = 9091;
+            DatagramSocket ds = new DatagramSocket();
+            InetAddress ip = InetAddress.getLocalHost();
+            byte buffer[] = null;
+            buffer = message.getBytes();
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, ip, port);
+            ds.send(packet);
+            ds.close();
         } catch (Exception e) {
+            System.out.println("Alerta! Errores encontrados durante ejecución");
         }
     }
 
+    public static void sendToVirtualDevice(String message){
+        
+    }
+
     static class PostHandler implements HttpHandler {
+
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             // Agregar encabezados CORS
@@ -57,10 +75,12 @@ public class Server {
                 StringBuilder hexResponse = new StringBuilder();
                 for (int i = 0; i < 24; i++) {
                     hexResponse.append(Integer.toHexString((int) (Math.random() * 16)));
-                }                
-                
+                }
+
                 response = hexResponse.toString();
                 response = "FFF000000000000000042804";
+                // Envio al Driver
+                sendToDriver(response);
                 exchange.sendResponseHeaders(200, response.length());
                 OutputStream os = exchange.getResponseBody();
                 os.write(response.getBytes());
--- Driver/src/Driver.java
+++ Driver/src/Driver.java
@@ -1,16 +1,51 @@
+
 import com.fazecast.jSerialComm.*;
 import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
 public class Driver {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SocketException {
+        // Como recibe el driver un mensaje de app -- Con un udp server corriendo
+        String protocol = "UDP";
+        String server = "127.0.0.1";
+        int port = 9091;
+        DatagramSocket socket = new DatagramSocket(port);
+        byte[] input = new byte[65535];
+        DatagramPacket receivePacket = null;
+
+        try {
+            receivePacket = new DatagramPacket(input, input.length);
+            socket.receive(receivePacket);
+            InetAddress c_address = receivePacket.getAddress();
+            int port_c = receivePacket.getPort();
+
+            String mess = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("< " + c_address + " app dice: " + mess);
+
+            /*
+                String resS = realizaOperacion(mess);
+                System.out.println("> " + receivePacket.getAddress() + " server " + "[" + LocalDate.now() + " " + LocalTime.now() + "] " + protocol + ": " + resS);
+                byte[] sendData = resS.getBytes(StandardCharsets.UTF_8);
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, c_address, port_c);
+                socket.send(sendPacket);
+             */
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Mandar mensaje hacia la pico por medio de UART
+
         SerialPort comPort = SerialPort.getCommPort("COM4");
         comPort.setComPortParameters(115200, 8, SerialPort.ONE_STOP_BIT, SerialPort.NO_PARITY);
         comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 1000, 0);
-        
+
         // comPort.openPort();
         // Diferencia entre READ_BLOCKING y READ_SEMI_BLOCKING ...
         // comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
-
         if (comPort.openPort()) {
             System.out.println("Puerto abierto exitosamente.");
 
@@ -24,9 +59,9 @@ public class Driver {
                 // Enviar el mensaje
                 salida.write(mensaje.getBytes());
                 salida.flush();
-                
+
                 System.out.println("Mensaje enviado: " + mensaje);
-                
+
                 // Esperar un poco para asegurar que el mensaje se envíe completamente
                 Thread.sleep(1000);
 
@@ -38,7 +73,7 @@ public class Driver {
                 } else {
                     System.out.println("No se recibió respuesta.");
                 }
-                
+
             } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
             } finally {
@@ -48,11 +83,19 @@ public class Driver {
             }
         } else {
             System.out.println("No se pudo abrir el puerto.");
-        }        
+        }
     }
 
     public static void sendMessage(PrintWriter output, String message) {
         output.println(message);
         output.flush();
-    }    
+    }
+
+    public static void receiveMessageFromApp() {
+        //A UDP Server always running
+    }
+
+    public static void receiveMessageFromUART() {
+
+    }
 }
